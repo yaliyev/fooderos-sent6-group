b7428500d1c601d1c851eb0734606b1b408c8ee0
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2024-01-22 21:39:07 +0400

    backend start application configuration

---
+++ server/src/main/java/az/developia/fooderos/sent6group/FooderosSent6GroupFullStackApplicationClass.java
@@ -0,0 +1,12 @@
+package az.developia.fooderos.sent6group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooderosSent6GroupFullStackApplicationClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooderosSent6GroupFullStackApplicationClass.class, args);
+	}
+}
