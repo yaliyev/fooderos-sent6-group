a25eee3ba072fca82472ed2a530b5c0cbebc21a6
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2024-02-06 20:38:02 +0400

    count price

--- server/src/main/resources/static/js/basket.js
+++ server/src/main/resources/static/js/basket.js
@@ -1,145 +1,203 @@
-let basketString  = localStorage.getItem('fooderos-sent6-basket');
-
+let basketString = localStorage.getItem("fooderos-sent6-basket");
 
 let basketFoods = [];
 
 let basketFoodsObjects = [];
 
+let basketFoodsTableElement = document.getElementById("basket-foods-table");
 
-let basketFoodsTableElement = document.getElementById('basket-foods-table');
 
-if(basketString != null){
-     basketFoods = JSON.parse(basketString);
+let totalPriceElement = document.getElementById('total-price');
+
+if (basketString != null) {
+  basketFoods = JSON.parse(basketString);
 }
 
+async function getBasketFoods() {
+  for (let i = 0; i < basketFoods.length; i++) {
+    let basketFoodItem = basketFoods[i];
+
+    let foodId = basketFoodItem.id;
+
+    let requestStatus = null;
+
+    let resultFood = await fetch(
+      `http://localhost:3000/api/foods/${foodId}`
+    ).then((response) => {
+      requestStatus = response.status;
+
+      return response.json();
+    });
+
+    if (requestStatus == 200) {
+      basketFoodsObjects.push(resultFood);
+    }
+  }
+}
+
+async function insertBasketData(reload = false) {
+  basketFoodsTableElement.innerHTML = "";
+
+  if (reload) {
+    await getBasketFoods();
+  }
+
+  for (let i = 0; i < basketFoodsObjects.length; i++) {
+    let basketFoodsObject = basketFoodsObjects[i];
+
+    let tr = document.createElement("tr");
+
+    let idTd = document.createElement("td");
+
+    let nameTd = document.createElement("td");
+
+    let imageTd = document.createElement("td");
+
+    let image = document.createElement("img");
+
+    let priceTd = document.createElement("td");
+
+    let quantityTd = document.createElement("td");
+
+    let summaryTd = document.createElement("td");
+
+    let quantityInput = document.createElement("input");
 
 
-async function getBasketFoods(){
 
-     for(let i = 0; i < basketFoods.length;i++){
+    let actionTd = document.createElement("td");
+
+    let deleteButton = document.createElement("button");
+
+    idTd.innerText = basketFoodsObject.id;
+
+    nameTd.innerText = basketFoodsObject.name;
+
+    image.src = basketFoodsObject.coverImg;
+
+    image.style.width = "150px";
+
+    image.style.height = "150px";
+
+    image.style.objectFit = "cover";
+
+    priceTd.innerText = basketFoodsObject.price + " AZN";
+
+    quantityInput.setAttribute("class","form-control");
+
+    quantityInput.style.width = "100px";
+
+    quantityInput.placeholder = "Say:";
+
+    quantityInput.setAttribute('id',`quantity${basketFoodsObject.id}`)
+
+    quantityInput.value = 0;
 
 
-          let basketFoodItem = basketFoods[i];
 
-          let foodId = basketFoodItem.id;
 
-          let requestStatus = null;
 
-          let resultFood = await fetch(`http://localhost:3000/api/foods/${foodId}`).then(response=>{
 
-               requestStatus = response.status;
 
-               return response.json();
-          })
+    deleteButton.innerText = "Delete";
 
-          if(requestStatus == 200){
-              basketFoodsObjects.push(resultFood);
+    deleteButton.setAttribute("class", "btn btn-danger");
+
+    let basketFoodsObjectIndex = -1;
+
+    let basketFoodIndex = -1;
+
+
+    quantityInput.addEventListener("input",function(){
+
+      summaryTd.innerText = Number(quantityInput.value) * basketFoodsObject.price + " AZN";
+
+      countTotalPrice();
+
+
+
+    });
+
+    deleteButton.addEventListener("click", function () {
+
+     
+
+
+      basketFoodsObjects.find((iteratedBasketFoodObject, index) => {
+        if (iteratedBasketFoodObject.id == basketFoodsObject.id) {
+          basketFoodsObjectIndex = index;
+        }
+      });
+
+
+      basketFoodsObjects.splice(basketFoodsObjectIndex,1);
+
+
+      basketFoods.find((iteratedBasketFood, index) => {
+          if (iteratedBasketFood.id == basketFoodsObject.id) {
+            basketFoodIndex = index;
           }
+        });
   
-          
-          
+  
+        basketFoodsObjects.splice(basketFoodsObjectIndex,1);
+        basketFoods.splice(basketFoodIndex,1);
 
 
-     }
+    localStorage.setItem('fooderos-sent6-basket',JSON.stringify(basketFoods));
+
+    Swal.fire({
+     icon: "success",
+     title: "Basket",
+     text: "Deleted from Basket",
+     timer: 1200
+   });
+
+    insertBasketData(false);
 
 
+
+    });
+
+    
+    
+
+
+
+    imageTd.appendChild(image);
+
+    quantityTd.appendChild(quantityInput);
+
+    actionTd.appendChild(deleteButton);
+
+    tr.appendChild(idTd);
+    tr.appendChild(nameTd);
+    tr.appendChild(imageTd);
+    tr.appendChild(priceTd);
+    tr.appendChild(quantityTd);
+    tr.appendChild(summaryTd);
+    tr.appendChild(actionTd);
+
+    basketFoodsTableElement.appendChild(tr);
+  }
 }
 
-async function insertBasketData(reload = false){
+function countTotalPrice(){
+
+  totalPriceElement.innerText = 0;
+
+  let sum = 0;
+
+  for(let i = 0; i < basketFoodsObjects.length;i++){
+    let basketFoodsObject = basketFoodsObjects[i];
 
 
-     basketFoodsObjects = [];
-     basketFoodsTableElement.innerHTML = "";
-     
+    sum += Number(document.getElementById(`quantity${basketFoodsObject.id}`).value) * basketFoodsObject.price;
 
-     if(reload){
+  }
 
-          await getBasketFoods();
+  totalPriceElement.innerText = sum;
 
-     }
-
-
-     for(let i = 0; i < basketFoodsObjects.length;i++){
-
-      let basketFoodsObject = basketFoodsObjects[i];    
-
-
-     //      <!-- <tr>
-     //      <td>1</td>
-     //      <td>Hamburger</td>
-     //      <td>
-     //           <img src="https://s7d1.scene7.com/is/image/mcdonalds/Header_Hamburger_832x472:1-3-product-tile-desktop?wid=763&hei=472&dpr=off" style="width: 150px;height: 150px;object-fit: cover;" alt="">
-     //      </td>
-     //      <td>
-     //           4.5
-     //      </td>
-     //      <td>
-     //           <button class="btn btn-danger">Delete</button>
-     //      </td>
-     // </tr> -->
-
-
-     let tr = document.createElement("tr");
-
-     let idTd = document.createElement("td");
-
-     let nameTd = document.createElement("td");
-
-     let imageTd = document.createElement("td");
-
-     let image = document.createElement("img");
-
-     let priceTd = document.createElement("td");
-
-     let actionTd = document.createElement("td");
-
-     let deleteButton = document.createElement("button");
-
-
-     idTd.innerText = basketFoodsObject.id;
-
-     nameTd.innerText = basketFoodsObject.name;
-
-     image.src = basketFoodsObject.coverImg;
-
-     image.style.width = "150px";
-
-     image.style.height = "150px";
-
-     image.style.objectFit = "cover";
-
-     priceTd.innerText = basketFoodsObject.price + " AZN";
-
-     deleteButton.innerText = "Delete";
-
-     deleteButton.setAttribute("class","btn btn-danger");
-
-
-
-     imageTd.appendChild(image);
-
-     actionTd.appendChild(deleteButton);
-
-
-     tr.appendChild(idTd);
-     tr.appendChild(nameTd);
-     tr.appendChild(imageTd);
-     tr.appendChild(priceTd);
-     tr.appendChild(actionTd);
-
-
-     basketFoodsTableElement.appendChild(tr);
-
-     
-
-
-
-
-     }
 }
 
 insertBasketData(true);
-
-
-
-
